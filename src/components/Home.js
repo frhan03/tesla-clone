91b5781101ffd550995f9d6303b19b643eec7690
frhan03 <95188563+frhan03@users.noreply.github.com>
2024-03-04 02:51:23 +0800

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import styled from 'styled-components'
+import Section from './Section'
+
+function Home() {
+  return (
+    <Container>
+            <Section
+                title="Model S"
+                description="Order online for touchup delivery"
+                backgroundImg="model-s.jpg"
+                leftBtnText="Custom order"
+                rightBtnText="Existing Inventory"
+            />
+            <Section
+                title="Model Y"
+                description="Order online for touchup delivery"
+                backgroundImg="model-y.jpg"
+                leftBtnText="Custom order"
+                rightBtnText="Existing Inventory"
+            />
+            <Section
+                title="Model 3"
+                description="Order online for touchup delivery"
+                backgroundImg="model-3.jpg"
+                leftBtnText="Custom order"
+                rightBtnText="Existing Inventory"
+            />
+            <Section
+                title="Model X"
+                description="Order online for touchup delivery"
+                backgroundImg="model-x.jpg"
+                leftBtnText="Custom order"
+                rightBtnText="Existing Inventory"
+            />
+            <Section
+                title="Lowest Cost Solar Panel"
+                description="Money-back guarantee"
+                backgroundImg="solar-panel.jpg"
+                leftBtnText="Order NOW"
+                rightBtnText="Learn more"
+            />
+            <Section
+                title="Solar for New Roofs"
+                description="Solar Roof Costs Less than a New Roof Plus Solar Panels"
+                backgroundImg="solar-roof.jpg"
+                leftBtnText="Order NOW"
+                rightBtnText="Learn more"
+            />
+            <Section
+                title="Accessories"
+                description=""
+                backgroundImg="accessories.jpg"
+                leftBtnText="Shop Now"
+            />
+        </Container>
+  )
+}
+
+export default Home
+
+const Container = styled.div`
+    height: 100vh;
+`
